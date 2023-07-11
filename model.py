7ea5dea7b7e45ae0e05bbcfd36dd2e6a4eed7399
InvisCo <20621549+InvisCo@users.noreply.github.com>
2023-07-11 17:08:39 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Tuple
+from typing import List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -10,56 +10,61 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
+FEATURE_INFO_PATH = Path("data/features.yaml")
+DATA_PATH = Path("data/train.csv")
+
 
 class FeatureInfo:
-    def __init__(self, yaml_path: Path):
-        self.feature_data = yaml.load(yaml_path)
+    def __init__(self, yaml_path: Path) -> None:
+        with yaml_path.open("r") as f:
+            self.__feature_data = yaml.load(f.read(), Loader=yaml.SafeLoader)
 
-    def get_type(self, feature):
-        return self.feature_data[feature]["type"]
+    def get_type(self, feature: str) -> str:
+        return self.__feature_data[feature].get("type")
 
-    def get_min(self, feature):
-        return self.feature_data[feature].get("min")
+    def get_input(self, feature: str) -> str:
+        return self.__feature_data[feature].get("input")
 
-    def get_max(self, feature):
-        return self.feature_data[feature].get("max")
+    def get_label(self, feature: str) -> str:
+        return self.__feature_data[feature].get("label")
 
-    def get_values(self, feature):
-        return self.feature_data[feature].get("values")
+    def get_min(self, feature: str) -> Optional[int]:
+        return int(self.__feature_data[feature].get("min"))
 
-    def get_labels(self, feature):
-        return self.feature_data[feature].get("labels")
+    def get_max(self, feature: str) -> Optional[int]:
+        return int(self.__feature_data[feature].get("max"))
 
-    def get_na(self, feature):
-        return self.feature_data[feature].get("NA")
+    def get_values(self, feature: str) -> Optional[List[str]]:
+        return self.__feature_data[feature].get("values")
 
-    def get_features(self):
-        return list(self.feature_data.keys())
+    def get_labels(self, feature: str) -> Optional[List[str]]:
+        return self.__feature_data[feature].get("labels")
 
-    def get_numerical_features(self):
+    def get_na(self, feature: str) -> Optional[str]:
+        return self.__feature_data[feature].get("NA")
+
+    def get_features(self) -> List[str]:
+        return list(self.__feature_data.keys())
+
+    def get_numerical_features(self) -> List[str]:
         return [
             feature
-            for feature in self.feature_data
+            for feature in self.__feature_data
             if self.get_type(feature) == "numerical"
         ]
 
-    def get_categorical_features(self):
+    def get_categorical_features(self) -> List[str]:
         return [
             feature
-            for feature in self.feature_data
+            for feature in self.__feature_data
             if self.get_type(feature) == "categorical"
         ]
 
-    def get_value_label_map(self):
-        output = {}
-        for feature in self.get_categorical_features():
-            output.update(
-                {
-                    self.get_values(feature): self.get_labels(feature),
-                    self.get_labels(feature): self.get_values(feature),
-                }
-            )
-        return output
+    def map_label_to_value(self, feature: str, label: str) -> str:
+        return self.get_values(feature)[self.get_labels(feature).index(label)]
+
+    def map_value_to_label(self, feature: str, value: str) -> str:
+        return self.get_label(feature)[self.get_value(feature).index(value)]
 
 
 def process_data(
@@ -88,22 +93,22 @@ def process_data(
     )
 
 
-def generate_models(filename: str, feature_info: FeatureInfo):
-    # Set filepath to the data
-    filepath = Path(filename)
-
+def generate_models(file: Path, feature_info: FeatureInfo):
+    # https://scikit-learn.org/stable/auto_examples/compose/plot_column_transformer_mixed_types.html#column-transformer-with-mixed-types
+    # Assign numerical features to MinMaxScaler and categorical features to OneHotEncoder
     preprocessor = ColumnTransformer(
         transformers=[
             ("num", MinMaxScaler(), feature_info.get_numerical_features()),
             ("cat", OneHotEncoder(), feature_info.get_categorical_features()),
         ]
     )
+    # Create model pipeline where data is preprocessed and then fed into a LinearRegression model
     model = Pipeline(
         steps=[("preprocessor", preprocessor), ("classifier", LinearRegression())]
     )
 
     # Process data and train model
-    X_train, X_test, y_train, y_test = process_data(filepath)
+    X_train, X_test, y_train, y_test = process_data(file, feature_info.get_features())
     model.fit(X_train, y_train)
 
     # Test model
