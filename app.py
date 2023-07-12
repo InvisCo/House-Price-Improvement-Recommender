2a2b0c0aaa056cbee01812b6174c6f37f8ac5f36
InvisCo <20621549+InvisCo@users.noreply.github.com>
2023-07-12 17:57:11 -0400

    Update with prediction

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-from model import DATA_PATH, FEATURE_INFO_PATH, FeatureInfo, generate_models
+from model import (
+    DATA_PATH,
+    FEATURE_INFO_PATH,
+    FeatureInfo,
+    find_price_increases,
+    generate_models,
+)
+
 
 # Title and description
-st.image("https://drive.google.com/uc?export=view&id=1ijBEINIw0Uv6u9imv7geshpPhYU4L2yy")
+st.image("cover.png")
 st.title("Property Improvement Predictor")
-st.write("Predict the resale value of properties based on renovations")
+st.write("Suggests property improvements to increase resale value.")
 
 # Load feature info
 FEATURE_INFO = FeatureInfo(FEATURE_INFO_PATH)
@@ -15,53 +22,69 @@ FEATURE_INFO = FeatureInfo(FEATURE_INFO_PATH)
 # Generate models at the start of the app
 model, test_results = generate_models(DATA_PATH, FEATURE_INFO)
 
-# Property features form
-with st.expander("Property Features", expanded=True):
-    st.header("Enter Property Details")
 
+def feature_average(feature):
+    """Returns the average value for a feature."""
+    return (
+        FEATURE_INFO.get_max(feature) - FEATURE_INFO.get_min(feature)
+    ) // 2 + FEATURE_INFO.get_min(feature)
+
+
+# Property features form
+st.header("Look up renovation suggestions for your property:")
+with st.form("input"):
     input_data = {}
     # Generate input fields
-    for feature in FEATURE_INFO.get_features():
+        # Generate input fields based on feature input type
+        # Number input
         if FEATURE_INFO.get_input(feature) == "number_input":
             input_data[feature] = st.number_input(
                 label=FEATURE_INFO.get_label(feature),
                 min_value=FEATURE_INFO.get_min(feature),
                 max_value=FEATURE_INFO.get_max(feature),
-                value=2000,
+                # Set default value to the average value
+                value=feature_average(feature),
             )
-
+        # Slider input
         elif FEATURE_INFO.get_input(feature) == "slider":
             input_data[feature] = st.slider(
                 label=FEATURE_INFO.get_label(feature),
                 min_value=FEATURE_INFO.get_min(feature),
                 max_value=FEATURE_INFO.get_max(feature),
-                value=1,
+                value=feature_average(feature),
             )
-
+        # Selectbox input
         elif FEATURE_INFO.get_input(feature) == "selectbox":
             input_data[feature] = st.selectbox(
                 label=FEATURE_INFO.get_label(feature),
                 options=FEATURE_INFO.get_labels(feature),
             )
-
+        # Select slider input
         elif FEATURE_INFO.get_input(feature) == "select_slider":
             input_data[feature] = st.select_slider(
                 label=FEATURE_INFO.get_label(feature),
                 options=FEATURE_INFO.get_labels(feature),
             )
-
+        # Radio input
         elif FEATURE_INFO.get_input(feature) == "radio":
             input_data[feature] = st.radio(
                 label=FEATURE_INFO.get_label(feature),
                 options=FEATURE_INFO.get_labels(feature),
             )
 
+    submit = st.form_submit_button(type="primary", help="Find renovation suggestions")
 
-# Submit button
-if st.button("Predict"):
-    # Convert labels into values for categorical features
-    for feature in FEATURE_INFO.get_categorical_features():
-        input_data[feature] = FEATURE_INFO.map_label_to_value(
-            feature, input_data[feature]
-        )
+    if submit:
+        with st.spinner("Predicting..."):
+            # Copy input data to avoid modifying on user action
+            data_submitted = input_data.copy()
+
+            # Convert labels into values for categorical features
+            for feature in FEATURE_INFO.get_categorical_features():
+                data_submitted[feature] = FEATURE_INFO.map_label_to_value(
+                    feature, data_submitted[feature]
+                )
+
+            # Predict the price increase
+            predictions = find_price_increases(model, data_submitted, FEATURE_INFO)
 
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -17,8 +17,12 @@ DATA_PATH = Path("data/train.csv")
 class FeatureInfo:
     def __init__(self, yaml_path: Path) -> None:
         with yaml_path.open("r") as f:
-            self.__feature_data = yaml.load(f.read(), Loader=yaml.SafeLoader)
+            # Load feature data from yaml file
+            self.__feature_data: Dict[str, Dict[str, Any]] = yaml.load(
+                f.read(), Loader=yaml.SafeLoader
+            )
 
+    # Getters
     def get_type(self, feature: str) -> str:
         return self.__feature_data[feature].get("type")
 
@@ -43,6 +47,10 @@ class FeatureInfo:
     def get_na(self, feature: str) -> Optional[str]:
         return self.__feature_data[feature].get("NA")
 
+    def is_renovatable(self, feature: str) -> bool:
+        return self.__feature_data[feature].get("renovation")
+
+    # Getters for lists of features
     def get_features(self) -> List[str]:
         return list(self.__feature_data.keys())
 
@@ -60,6 +68,12 @@ class FeatureInfo:
             if self.get_type(feature) == "categorical"
         ]
 
+    def get_renovatable_features(self) -> List[str]:
+        return [
+            feature for feature in self.__feature_data if self.is_renovatable(feature)
+        ]
+
+    # Mapping functionss
     def map_label_to_value(self, feature: str, label: str) -> str:
         return self.get_values(feature)[self.get_labels(feature).index(label)]
 
@@ -72,6 +86,9 @@ def process_data(
 ) -> Tuple[dict[str, pd.DataFrame], dict[str, pd.DataFrame]]:
     # Read data from csv file
     df = pd.read_csv(filepath)
+
+    # Shuffle data and reset incices
+    df = df.sample(frac=1, random_state=3654).reset_index(drop=True)
 
     # Sum values in columns "BsmtFinSF1" and "BsmtFinSF2" to "BsmtFinSF"
     df["BsmtFinSF"] = df["BsmtFinSF1"] + df["BsmtFinSF2"]
@@ -99,7 +116,11 @@ def generate_models(file: Path, feature_info: FeatureInfo):
     preprocessor = ColumnTransformer(
         transformers=[
             ("num", MinMaxScaler(), feature_info.get_numerical_features()),
-            ("cat", OneHotEncoder(), feature_info.get_categorical_features()),
+            (
+                "cat",
+                OneHotEncoder(handle_unknown="ignore"),
+                feature_info.get_categorical_features(),
+            ),
         ]
     )
     # Create model pipeline where data is preprocessed and then fed into a LinearRegression model
@@ -107,8 +128,9 @@ def generate_models(file: Path, feature_info: FeatureInfo):
         steps=[("preprocessor", preprocessor), ("classifier", LinearRegression())]
     )
 
-    # Process data and train model
+    # Process data
     X_train, X_test, y_train, y_test = process_data(file, feature_info.get_features())
+    # Train model
     model.fit(X_train, y_train)
 
     # Test model
@@ -120,3 +142,75 @@ def generate_models(file: Path, feature_info: FeatureInfo):
 
     return model, test_results
 
+
+def find_price_increases(
+    model: Pipeline,
+    input_data: dict,
+    feature_info: FeatureInfo,
+):
+    # Convert input data into a dataframe with the correct columns
+    input_df = pd.DataFrame(index=[0], columns=feature_info.get_features())
+    for feature, value in input_data.items():
+        # Assign value to feature in dataframe
+        input_df[feature] = value
+
+    # Get list of renovatable features
+    renovatable_features = feature_info.get_renovatable_features()
+
+    results = []
+    # For each renovatable feature...
+    for feature in renovatable_features:
+        # Make a copy of the data
+        X_up = input_df.copy()
+        X_down = input_df.copy()
+
+        # Determine the step
+        if feature_info.get_type(feature) == "numerical":
+            # If the feature is small (likely number of rooms), step is 1
+            if feature_info.get_max(feature) <= 10:
+                step = 1
+            # If feature value is below 100, step is entire value
+            elif X_up[feature][0] < 100:
+                step = X_up[feature][0]
+            # Else, step is 100
+            else:
+                step = 100
+
+            # Increment and decrement the feature value by the step
+            X_up[feature] += step
+            X_down[feature] -= step
+        else:  # feature type = categorical
+            values = feature_info.get_values(feature)
+            current_index = values.index(input_df[feature][0])
+
+            # Get the next and previous values in the list of values
+            next_index = min(current_index + 1, len(values) - 1)
+            prev_index = max(current_index - 1, 0)
+
+            # Set the new values
+            X_up[feature] = values[next_index] if next_index != current_index else None
+            X_down[feature] = (
+                values[prev_index] if prev_index != current_index else None
+            )
+
+        # Predict the new price and calculate the change
+        for X in (X_up, X_down):
+            try:
+                # Predict the price
+                new_price = model.predict(X)[0]
+                old_price = model.predict(input_df)[0]
+                # Calculate the change
+                change = new_price - old_price
+
+                # Only store the result if the price increased
+                if change > 0:
+                    # Round the change to the nearest integer
+                    change = int(round(change, 0))
+                    results.append((feature, change))
+            except ValueError:
+                # Exception raised if X contains a categorical value that is not in the training data
+                continue
+
+    # Sort the results by price change in descending order and return them
+    results.sort(key=lambda x: x[1], reverse=True)
+    return results
